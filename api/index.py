37da0b088a0837cc2694df14693551f074bfca89
zhiqiangqian <qianzhiqiang.qzq@alibaba-inc.com>
2024-09-14 14:33:14 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,12 +1,20 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+# from cowpy import cow
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
+#
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+#         self.wfile.write(message.encode())
+#         return
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
-        return
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
